9809b3cc917c105f5573a951f98b067c590e7421
Iury Chagas <ikarochagas.ig@gmail.com>
2022-05-29 20:26:22 -0300

    att

---
+++ script.js
@@ -0,0 +1,136 @@
+// VARIÁVEIS DE CONTROLE DE INTERFACE
+
+let yourVote = document.querySelector('.division-1-1 span');
+let politicalOffice = document.querySelector('.division-1-2 span');
+let generalDesc = document.querySelector('.division-1-4');
+let generalWarning = document.querySelector('.division-2');
+let lateral = document.querySelector('.division-1-right');
+let numbers = document.querySelector('.division-1-3');
+
+// VARIÁVEIS DE CONTROLE DE AMBIENTE
+
+let actualStep = 0;  // Etapa atual
+let number = ''; // Os números serão preenchidos nessa variável
+let whiteVote = false;
+let votes = [];
+
+// VARIÁVEIS DE AMBIENTE FINALIZADAS
+
+function startStep() { //ETAPA DE VOTAÇÃO ABERTA
+
+    let step = steps[actualStep];
+
+    let numberHtml = '';
+    number = '';
+    whiteVote = false;
+
+
+    for(let i=0;i<step.numbers;i++) {
+        if (i === 0) {
+            numberHtml += '<div class="number flash"></div>';
+            } else {
+            numberHtml += '<div class="number"></div>';
+     }
+}
+
+    yourVote.style.display = 'none';
+    politicalOffice.innerHTML = step.title;
+    generalDesc.innerHTML = '';
+    generalWarning.style.display = 'none';
+    lateral.innerHTML = '';
+    numbers.innerHTML = numberHtml;
+} // ETAPA DE VOTAÇÃO FECHADA
+
+function refreshInterface() {
+    let step = steps[actualStep]
+        let candidate = step.candidates.filter((item)=>{
+
+            if (item.number === number) {
+                return true;
+            } else  {
+                return false;
+            }
+            
+        });
+        
+        if (candidate.length > 0) {
+            candidate = candidate[0];
+            yourVote.style.display = 'block';
+            generalWarning.style.display = 'block';
+            generalDesc.innerHTML = `Nome: ${candidate.name}<br/>Partido: ${candidate.camelCase}`;
+            
+            
+            let photosHtml = '';
+            for(let i in candidate.photos) {
+            photosHtml += `<div class="division-1-image"><img src="images/${candidate.photos[i].url}""alt=">${candidate.photos[i].subtitle}</div>`;
+            
+            }
+            lateral.innerHTML = photosHtml;
+        } else {
+            yourVote.style.display = 'block';
+            generalWarning.style.display = 'block';
+            generalDesc.innerHTML = '<div class="large-warning flash">VOTO NULO</div>';
+        }
+}
+
+function chooseNumber(n) {
+    let numberElement = document.querySelector('.number.flash');
+
+    if(numberElement !== null) {
+        numberElement.innerHTML = n;
+        number = `${number}${n}`;
+
+        numberElement.classList.remove('flash');
+
+        if (numberElement.nextElementSibling !== null) {
+            numberElement.nextElementSibling.classList.add('flash');
+        } else {
+
+            refreshInterface();
+        } 
+    }
+}
+
+function white() {
+    if (number === '') {
+        whiteVote = true;
+        yourVote.style.display = 'block';
+        generalWarning.style.display = 'block';
+        numbers.innerHTML = '';
+        generalDesc.innerHTML = '<div class="large-warning flash">VOTO EM BRANCO</div>';
+        lateral.innerHTML= '';
+    }
+}
+
+function clear() {
+    startStep();
+}
+
+function confirm() {
+    if(whiteVote === true) { // O botão confirmar só funciona se o botão votar em branco estiver verdadeiro "true"
+       voteConfirmed = true;
+       votes.push({
+           step: steps[actualStep].title
+           vote: 'whiteVote'
+       })
+    } else if(number.length === steps.numbers) {
+       voteConfirmed = true;
+       votes.push({
+        step: steps[actualStep].title
+        vote: 'number'
+    })
+ }
+  if (voteConfirmed) {
+        actualStep++;
+        if (steps[actualStep] !== undefined) {
+            startStep()
+        } else {
+            document.querySelector('.screen').innerHTML = '<div class="giant-warning flash">VOTO EM BRANCO</div>'
+        }
+  }
+}
+
+startStep()
+
+
+
---
+++ steps.js
@@ -0,0 +1,50 @@
+let steps = [
+    {
+        title: 'VEREADOR',
+        numbers: 5,
+        candidates: [
+            {
+                number: '38111',
+                name: 'Fulano de Tal',
+                camelCase: 'STA',
+                photos: [
+                    {url: 'candidato-1', subtitle: 'Vereador', small: true}
+                ]
+            },
+            {
+                number: '77222',
+                name: 'Beltrano da Silva',
+                camelCase: 'DEFG',
+                photos: [
+                    {url: 'candidato-2', subtitle: 'Vereador',small: true}
+                ]
+            }
+        ]
+    },
+    {
+        title: 'PREFEITO',
+        numbers: 2,
+        candidates: [
+            {
+                number: '35',
+                name: 'João Bosco',
+                camelCase: 'STA',
+                vice: 'Cic',
+                photos: [
+                    {url: 'candidato-3', subtitle: 'Prefeito', small: true},
+                    {url: 'candidato-1', subtitle: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                number: '84',
+                name: 'Márcio Paulo',
+                camelCase: 'QWERTY',
+                vice: 'Zul',
+                photos: [
+                    {url: 'candidato-2', subtitle: 'Prefeito', small: true},
+                    {url: 'candidato-3', subtitle: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
